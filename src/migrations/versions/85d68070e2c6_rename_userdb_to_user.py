2b09a9a4ba1de7d0b9098604a029eafdd66401b8
bzadorozhnyi <bohzador@gmail.com>
2025-08-20 18:00:43 +0300

    PWL-35 applied ruff format;

--- src/migrations/versions/85d68070e2c6_rename_userdb_to_user.py
+++ src/migrations/versions/85d68070e2c6_rename_userdb_to_user.py
@@ -5,13 +5,14 @@ Revises: 194d3095b205
 Create Date: 2025-08-18 16:00:08.139666
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '85d68070e2c6'
-down_revision: Union[str, Sequence[str], None] = '194d3095b205'
+revision: str = "85d68070e2c6"
+down_revision: Union[str, Sequence[str], None] = "194d3095b205"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
