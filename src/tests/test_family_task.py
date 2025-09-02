335f551dd0e105f6bc96e88258a7fccefd88895e
bzadorozhnyi <bohzador@gmail.com>
2025-09-02 18:15:53 +0300

    PWL-51 added test for update endpoints;

--- src/tests/test_family_task.py
+++ src/tests/test_family_task.py
@@ -1,3 +1,4 @@
+import uuid
 from collections import Counter
 
 import jsonschema
@@ -329,6 +330,7 @@ async def test_paginated_list_family_tasks_multiple_pages(
 @pytest.mark.anyio
 async def test_update_family_task_success(
     async_client,
+    db_session,
     user_factory,
     family_factory,
     family_member_factory,
@@ -352,19 +354,351 @@ async def test_update_family_task_success(
     assert auth_response.status_code == status.HTTP_200_OK
     access_token = auth_response.json()["tokens"]["access_token"]
 
-    update_data = family_task_update_payload_factory(assignee_id=str(user2.id), done=True)
+    update_data = family_task_update_payload_factory(
+        assignee_id=str(user2.id), done=True
+    )
     response = await async_client.put(
         f"/api/tasks/{task.id}/",
         json=update_data,
         headers={"authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == status.HTTP_200_OK
-    data = response.json()
 
-    assert data["title"] == update_data["title"]
-    assert data["assignee"]["id"] == update_data["assignee_id"]
-    assert data["done"] == update_data["done"]
-    _assert_family_task_response_schema(data)
+    family_task = await db_session.scalar(
+        select(FamilyTask).where(FamilyTask.creator_id == user1.id)
+    )
+
+    assert family_task.title == update_data["title"]
+    assert str(family_task.assignee_id) == update_data["assignee_id"]
+    assert family_task.done == update_data["done"]
+    _assert_family_task_response_schema(response.json())
+
+
+@pytest.mark.anyio
+async def test_cannot_update_nonexisting_task(
+    async_client, user_factory, family_task_update_payload_factory
+):
+    """Test that user cannot update a non-existing task."""
+    user = user_factory()
+    payload = {"identifier": user.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = family_task_update_payload_factory()
+    response = await async_client.put(
+        f"/api/tasks/{str(uuid.uuid4())}/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.anyio
+async def test_update_family_task_only_creator(
+    async_client,
+    user_factory,
+    family_task_update_payload_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+):
+    """Test that only the creator can update the task."""
+    user1 = user_factory()
+    user2 = user_factory()
+
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=user1.id)
+    family_member_factory(family_id=family.id, user_id=user2.id)
+
+    task = family_task_factory(
+        family_id=family.id, creator_id=user1.id, assignee_id=user2.id
+    )
+
+    payload = {"identifier": user2.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = family_task_update_payload_factory()
+    response = await async_client.put(
+        f"/api/tasks/{task.id}/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.anyio
+async def test_cannot_update_with_invalid_assignee(
+    async_client,
+    user_factory,
+    family_task_update_payload_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+):
+    """Test that user cannot update a task with an invalid assignee."""
+    user1 = user_factory()
+    user2 = user_factory()
+
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=user1.id)
+
+    task = family_task_factory(
+        family_id=family.id, creator_id=user1.id, assignee_id=user1.id
+    )
+
+    payload = {"identifier": user1.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = family_task_update_payload_factory(
+        assignee_id=str(user2.id), done=True
+    )
+    response = await async_client.put(
+        f"/api/tasks/{task.id}/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.anyio
+async def test_cannot_update_with_empty_title(
+    async_client,
+    user_factory,
+    family_task_update_payload_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+):
+    """Test that user cannot update a task with an empty title."""
+    user = user_factory()
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=user.id)
+
+    task = family_task_factory(
+        family_id=family.id, creator_id=user.id, assignee_id=user.id
+    )
+
+    payload = {"identifier": user.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = family_task_update_payload_factory(title="")
+    response = await async_client.put(
+        f"/api/tasks/{task.id}/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.anyio
+async def test_cannot_update_task_creator(
+    async_client,
+    user_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+):
+    """Test that user cannot update a task's creator."""
+    user1 = user_factory()
+    user2 = user_factory()
+
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=user1.id)
+    family_member_factory(family_id=family.id, user_id=user2.id)
+
+    task = family_task_factory(
+        family_id=family.id, creator_id=user1.id, assignee_id=user1.id
+    )
+
+    payload = {"identifier": user1.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = {"creator_id": str(user2.id)}
+    response = await async_client.put(
+        f"/api/tasks/{task.id}/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize(
+    "initial_done, update_done",
+    [
+        (False, False),
+        (False, True),
+        (True, False),
+        (True, True),
+    ],
+)
+async def test_update_task_done_success_by_creator(
+    async_client,
+    db_session,
+    user_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+    initial_done,
+    update_done,
+):
+    """Test that creator can update task's done status."""
+    creator = user_factory()
+    assignee = user_factory()
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=creator.id)
+    family_member_factory(family_id=family.id, user_id=assignee.id)
+
+    task = family_task_factory(
+        family_id=family.id,
+        creator_id=creator.id,
+        assignee_id=assignee.id,
+        done=initial_done,
+    )
+
+    payload = {"identifier": creator.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = {"done": update_done}
+    response = await async_client.patch(
+        f"/api/tasks/{task.id}/done/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    family_task = await db_session.scalar(
+        select(FamilyTask).where(FamilyTask.creator_id == creator.id)
+    )
+
+    assert family_task.done == update_data["done"]
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize(
+    "initial_done, update_done",
+    [
+        (False, False),
+        (False, True),
+        (True, False),
+        (True, True),
+    ],
+)
+async def test_update_task_done_success_by_assignee(
+    async_client,
+    db_session,
+    user_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+    initial_done,
+    update_done,
+):
+    """Test that assignee can update task's done status."""
+    creator = user_factory()
+    assignee = user_factory()
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=creator.id)
+    family_member_factory(family_id=family.id, user_id=assignee.id)
+
+    task = family_task_factory(
+        family_id=family.id,
+        creator_id=creator.id,
+        assignee_id=assignee.id,
+        done=initial_done,
+    )
+
+    payload = {"identifier": assignee.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    assert auth_response.status_code == status.HTTP_200_OK
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = {"done": update_done}
+    response = await async_client.patch(
+        f"/api/tasks/{task.id}/done/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    family_task = await db_session.scalar(
+        select(FamilyTask).where(FamilyTask.creator_id == creator.id)
+    )
+
+    assert family_task.done == update_data["done"]
+
+
+@pytest.mark.anyio
+async def test_update_task_done_not_found(
+    async_client,
+    user_factory,
+):
+    """Test updating non-existing task returns 404."""
+    user = user_factory()
+    payload = {"identifier": user.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = {"done": True}
+    response = await async_client.patch(
+        f"/api/tasks/{str(uuid.uuid4())}/done/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.anyio
+async def test_update_task_done_forbidden(
+    async_client,
+    user_factory,
+    family_factory,
+    family_member_factory,
+    family_task_factory,
+):
+    """Test that a user who is neither creator nor assignee cannot update done."""
+    creator = user_factory()
+    assignee = user_factory()
+    other_user = user_factory()
+
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=creator.id)
+    family_member_factory(family_id=family.id, user_id=assignee.id)
+    family_member_factory(family_id=family.id, user_id=other_user.id)
+
+    task = family_task_factory(
+        family_id=family.id, creator_id=creator.id, assignee_id=assignee.id, done=False
+    )
+
+    payload = {"identifier": other_user.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    update_data = {"done": True}
+    response = await async_client.patch(
+        f"/api/tasks/{task.id}/done/",
+        json=update_data,
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 def _assert_family_task_response_schema(data):
