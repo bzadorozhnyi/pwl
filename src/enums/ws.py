e81a27b30a027e5ac657b53c0dd98b3d19844f89
bzadorozhnyi <bohzador@gmail.com>
2025-09-07 13:32:03 +0300

    PWL-63 removed unnecessary code;

--- src/enums/ws.py
+++ src/enums/ws.py
@@ -1,11 +1,6 @@
 from enum import StrEnum
 
 
-class WebSocketClientEvent(StrEnum):
-    CREATED_TASK = "created_task"
-    UPDATED_TASK = "updated_task"
-
-
 class WebSocketServerEvent(StrEnum):
     USER_CREATED_TASK = "user_created_task"
     USER_UPDATED_TASK = "user_updated_task"
