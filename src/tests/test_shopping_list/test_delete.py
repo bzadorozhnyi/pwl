fa07f83f8c0898b86ca80b1f181b5354fb78e840
bzadorozhnyi <bohzador@gmail.com>
2025-09-11 09:40:41 +0300

    PWL-65 ensured that deleting shopping list deletes its items;

--- src/tests/test_shopping_list/test_delete.py
+++ src/tests/test_shopping_list/test_delete.py
@@ -6,6 +6,7 @@ from httpx_ws import aconnect_ws
 from sqlmodel import select
 
 from models.shopping_list import ShoppingList
+from models.shopping_list_item import ShoppingListItem
 from tests.test_shopping_list.schemas_utils import (
     _assert_websocket_shopping_list_delete_event_response_schema,
 )
@@ -140,3 +141,48 @@ async def test_websocket_shopping_list_delete_success(
         assert response["data"]["id"] == str(shopping_list.id)
 
         _assert_websocket_shopping_list_delete_event_response_schema(response)
+
+
+@pytest.mark.anyio
+async def test_delete_shopping_list_also_deletes_items(
+    async_client,
+    db_session,
+    user_factory,
+    family_factory,
+    family_member_factory,
+    shopping_list_factory,
+    shopping_list_item_factory,
+):
+    """Test that deleting a shopping list also deletes its items."""
+    user = user_factory()
+    family = family_factory()
+    family_member_factory(family_id=family.id, user_id=user.id)
+    shopping_list = shopping_list_factory(family_id=family.id, creator_id=user.id)
+
+    item1 = shopping_list_item_factory(shopping_list_id=shopping_list.id)
+    item2 = shopping_list_item_factory(shopping_list_id=shopping_list.id)
+
+    payload = {"identifier": user.email, "password": "password"}
+    auth_response = await async_client.post("/api/auth/token/", json=payload)
+    access_token = auth_response.json()["tokens"]["access_token"]
+
+    response = await async_client.delete(
+        f"/api/shopping-lists/{shopping_list.id}/",
+        headers={"authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    shopping_list = await db_session.scalar(
+        select(ShoppingList).where(ShoppingList.id == shopping_list.id)
+    )
+    assert shopping_list is None
+
+    item1_db = await db_session.scalar(
+        select(ShoppingListItem).where(ShoppingListItem.id == item1.id)
+    )
+    item2_db = await db_session.scalar(
+        select(ShoppingListItem).where(ShoppingListItem.id == item2.id)
+    )
+    assert item1_db is None
+    assert item2_db is None
